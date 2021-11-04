8b2b4cb3c51d705d74d45e039eeabf48135fb652
sujith1233 <S545436@NWMISSOURI.EDU>
2021-11-04 00:57:30 -0500

    adeed

--- routes/hill.js
+++ routes/hill.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("tree", { title: "Search Results Tree" });
+  res.render("hill", { title: "Search Results hill" });
 });
 
 module.exports = router;
