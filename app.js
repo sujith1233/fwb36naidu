fecfcfa6d93347103e3300ed94b050beea236b17
sujith1233 <S545436@NWMISSOURI.EDU>
2021-11-04 00:49:58 -0500

    adeed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/addmod', addRouter);
+app.use('/addmod', addmodRouter);
 
 
 // catch 404 and forward to error handler
